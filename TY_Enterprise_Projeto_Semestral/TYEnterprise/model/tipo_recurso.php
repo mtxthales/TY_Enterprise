c86a91c083aff876dca5a406ce092cff7050013e
mtxthales <mtxthales@hotmail.com>
2016-07-30 05:27:19 -0300

    .

---
+++ TY_Enterprise_Projeto_Semestral/TYEnterprise/model/tipo_recurso.php
@@ -0,0 +1,24 @@
+<?php
+
+  class TipoRecurso{
+
+    private $codigo;
+    private $descricao;
+
+    function __construct($codigo){
+      $this->codigo = $codigo;
+    }
+
+    public function getCodigo(){
+      return $this->codigo;
+    }
+
+    public function setDesc($descricao){
+      $this->descricao = $descricao;
+    }
+
+    public function getDesc(){
+      return $this->descricao;
+    }
+  }
+?>
